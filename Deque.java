4a40a99bd9a28ca647d3bb1af30b54adedfca9dd
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-07-01 22:21:49 +0530

    use circular array

--- Deque.java
+++ Deque.java
@@ -1,16 +1,46 @@
+import java.lang.Object;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-    private int sz = 0;
+    private Item[] dq;
+    private int sz;
+    private int front;
+    private int rear;
 
     // construct an empty deque
     public Deque() {
+        dq = (Item[]) new Object[10];
+        sz = 0;
+        front=0;
+        rear=-1;
+    }
+
+    // resize circular array
+    private void resize() {
+        Item[] aux = (Item[]) new Object[dq.length * 2];
+        int newIndex = 0;
+        int oldIndex = front;
+
+        boolean rearReached = false;
+
+        while (!rearReached) {
+            rearReached = oldIndex % dq.length == rear;
+            aux[newIndex] = dq[oldIndex % dq.length];
+
+            newIndex++;
+            oldIndex++;
+        }
+        front = 0;
+        rear = dq.length - 1;
+        dq = aux;
 
     }
 
     // is the deque empty?
     public boolean isEmpty() {
+        if(front>rear)
+            return true;
         return false;
     }
 
